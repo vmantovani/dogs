dd8ead4b72e763452a1724c8aa6e4e395937fc02
Vinicius Mantovani <vmantovani@gmail.com>
2023-04-27 19:07:51 +0200

    feat: added lint config and clean up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,32 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended"
+    ],
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    },
+    "plugins": ["react", "react-hooks"],
+    "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    },
+    "env": {
+      "browser": true,
+      "amd": true,
+      "node": true
   },
-}
+    "rules": {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "no-unsafe-finally": "off",
+      "no-unused-vars": "off",
+      "react-refresh/only-export-components": "off"
+    }
+  }
